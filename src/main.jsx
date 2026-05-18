f94d944d9e30137f425cbdded0a43b36d949bbca
Stellazaz82 <stellaca82@gmail.com>
2026-05-18 10:41:29 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../schema-digitale.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
